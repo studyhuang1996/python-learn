6f10b8e97cdb6441683e1b6a76f2c222be1ad000
studyhuang1996 <1128hjy>
2020-10-19 23:46:54 +0800

    新增使用画决策树的图形代码

--- shizhan/tree/treePlotter.py
+++ shizhan/tree/treePlotter.py
@@ -1,3 +1,11 @@
+'''
+Author: your name
+Date: 2020-10-18 15:23:56
+LastEditTime: 2020-10-19 23:45:03
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: /python/shizhan/tree/treePlotter.py
+'''
 
 import matplotlib.pyplot as pyplot
 
@@ -21,6 +29,85 @@ def createPlot():
     pyplot.rcParams['font.sans-serif'] = ['Arial Unicode MS'] #解决中文乱码
     pyplot.show()
 
-createPlot()
+# createPlot()
     
 
+'''
+获取叶子节点的数目: 
+param {type} 
+return {type} 
+'''
+def getNumLeafs(myTree):
+    numLeafs=0
+    #得到根节点
+    firstStr = list(myTree.keys())[0]
+    #子节点集
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        #如果子集是字典类型，则还有子节点
+        if type(secondDict[key]).__name__=='dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs +=1
+    return numLeafs
+
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':
+            thisDepth = 1+ getTreeDepth(secondDict[key])
+        else:
+            thisDepth =1
+        if thisDepth > maxDepth : maxDepth = thisDepth
+    return maxDepth
+
+
+mydata = {'no surfacing': {0: 'no', 1: {'flippers': {0: 'no', 1: 'yes'}}}}
+
+# getNumLeafs(mydata)
+# print(getTreeDepth(mydata))
+
+def plotMidText(currentPr,parentPt,txtStr):
+    xMid =(parentPt[0]-currentPr[0])/2.0 + currentPr[0]
+    yMid =(parentPt[1]-currentPr[1])/2.0 + currentPr[1]
+    createPlot.ax1.text(xMid,yMid,txtStr)
+
+'''
+description: 
+param {type} 
+return {type} 
+'''
+def plotTree(myTree,parentPt,nodeTxt):
+    numLeafs =getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = list(myTree.keys())[0]
+    cntrPt = (plotTree.xOff + (1.0+float(numLeafs))/2.0/plotTree.totalW, 1.00)
+    plotMidText(cntrPt,parentPt,nodeTxt)
+    plotNode(firstStr,cntrPt,parentPt,decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff -1.0/plotTree.totalW
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':
+            plotTree(secondDict[key],cntrPt,str(key))
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+            plotNode(secondDict[key],(plotTree.xOff,plotTree.yOff),cntrPt,leafNode)
+            plotMidText((plotTree.xOff,plotTree.yOff),cntrPt,str(key))
+    plotTree.yOff =  plotTree.yOff + 1.0/ plotTree.totalD
+
+def createPlotTree(inTree):
+    fig = pyplot.figure(1,facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[],yticks=[])
+    createPlot.ax1 = pyplot.subplot(111,frameon=False,**axprops)
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5/plotTree.totalW; plotTree.yOff = 1.0;plotTree.vOff =5
+    plotTree(inTree,(0.5,1.0),'')
+    pyplot.rcParams['font.sans-serif'] = ['Arial Unicode MS'] #解决中文乱码
+    pyplot.show()    
+
+
+createPlotTree(mydata)
